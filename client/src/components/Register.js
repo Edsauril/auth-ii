4374b0514bf34d5eee9e821cbfb33ec932d7a6b4
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-11-29 17:50:25 -0500

    Login form created

---
+++ client/src/components/Register.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+const initialUser = {
+  username: "",
+  password: ""
+};
+
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { ...initialUser },
+      message: ""
+    };
+  }
+
+  submitHandler = () => {};
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={this.state.username}
+          />
+        </form>
+        <form onSubmit={this.submitHandler}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            value={this.state.password}
+          />
+        </form>
+        {this.state.message ? <h4>{this.state.message}</h4> : undefined}
+      </div>
+    );
+  }
+}
